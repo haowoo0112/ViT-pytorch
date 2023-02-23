7b6489a3c164d8c1b247d1557ac15eab0e1e83ce
haowoo0112 <haowoo0112@gmail.com>
2023-02-23 10:47:06 +0800

    feat: add model

--- models/model.py
+++ models/model.py
@@ -1,18 +1,36 @@
 import torch
 import torch.nn as nn
 import numpy as np
+import copy
+import math
+import models.configs as configs
 
 from torch.nn import CrossEntropyLoss, Dropout, Softmax, Linear, Conv2d, LayerNorm
+
+def np2th(weights, conv=False):
+    """Possibly convert HWIO to OIHW."""
+    if conv:
+        weights = weights.transpose([3, 2, 0, 1])
+    return torch.from_numpy(weights)
 
 class VisionTransformer(nn.Module):
     def __init__(self, img_size, patch_size):
         super().__init__()
 
         self.embeddings = Embeddings(img_size, patch_size, 3)
+        self.encoder = Encoder()
+        self.head = Linear(768, 10)
 
-    def forward(self, img):
+    def forward(self, img, labels=None):
         x = self.embeddings(img)
-        return x
+        x = self.encoder(x)
+        logits = self.head(x[:, 0])
+
+        # if labels is not None:
+        loss_fct = CrossEntropyLoss()
+        loss = loss_fct(logits.view(-1, self.num_classes), labels.view(-1))
+        return loss
+        # return x
 
 class Embeddings(nn.Module):
     """Construct the embeddings from patch, position embeddings.
@@ -38,3 +56,115 @@ class Embeddings(nn.Module):
         x = torch.cat((self.cls_token , x), dim=0)
         embeddings = x + self.position_embeddings
         print("embedding_shape: ", embeddings.shape)
+        return embeddings
+
+class Encoder(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.layer = nn.ModuleList()
+        self.encoder_norm = LayerNorm(768, eps=1e-6)
+        for _ in range(12):
+            layer = Block()
+            self.layer.append(copy.deepcopy(layer))
+    
+    def forward(self, hidden_states):
+        for layer_block in self.layer:
+            hidden_states = layer_block(hidden_states)
+        encoded = self.encoder_norm(hidden_states)
+        return encoded
+
+class Block(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.attention_norm = LayerNorm(768, eps=1e-6)
+        self.ffn_norm = LayerNorm(768, eps=1e-6)
+        self.ffn = Mlp()
+        self.attn = Attention()
+
+    def forward(self, x):
+        h = x
+        x = self.attention_norm(x)
+        x = self.attn(x)
+        x = x + h
+
+        h = x
+        x = self.ffn_norm(x)
+        x = self.ffn(x)
+        x = x + h
+        return x
+
+class Attention(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.num_attention_heads = 12
+        self.attention_head_size = int(768 / 12)
+        self.all_head_size = self.num_attention_heads * self.attention_head_size
+
+        self.query = Linear(768, self.all_head_size)
+        self.key = Linear(768, self.all_head_size)
+        self.value = Linear(768, self.all_head_size)
+
+        self.out = Linear(768, 768)
+
+        self.softmax = Softmax(dim=-1)
+    
+    def transpose_for_scores(self, x):
+        new_x_shape = x.size()[:-1] + (self.num_attention_heads, self.attention_head_size)
+        x = x.view(*new_x_shape)
+        # print(x.shape)
+        return x.permute(0, 2, 1, 3)
+
+    def forward(self, hidden_states):
+        mixed_query_layer = self.query(hidden_states)
+        mixed_key_layer = self.key(hidden_states)
+        mixed_value_layer = self.value(hidden_states)
+
+        query_layer = self.transpose_for_scores(mixed_query_layer)
+        key_layer = self.transpose_for_scores(mixed_key_layer)
+        value_layer = self.transpose_for_scores(mixed_value_layer)
+
+        attention_scores = torch.matmul(query_layer, key_layer.transpose(-1, -2))
+        attention_scores = attention_scores / math.sqrt(self.attention_head_size)
+        attention_probs = self.softmax(attention_scores)
+        context_layer = torch.matmul(attention_probs, value_layer)
+        context_layer = context_layer.permute(0, 2, 1, 3).contiguous()
+        new_context_layer_shape = context_layer.size()[:-2] + (self.all_head_size,)
+        context_layer = context_layer.view(*new_context_layer_shape)
+        attention_output = self.out(context_layer)
+
+        return attention_output
+
+def swish(x):
+    return x * torch.sigmoid(x)
+ACT2FN = {"gelu": torch.nn.functional.gelu, "relu": torch.nn.functional.relu, "swish": swish}
+
+class Mlp(nn.Module):
+    def __init__(self):
+        super(Mlp, self).__init__()
+        self.fc1 = Linear(768, 3072)
+        self.fc2 = Linear(3072, 768)
+        self.act_fn = ACT2FN["gelu"]
+
+        self._init_weights()
+
+    def _init_weights(self):
+        nn.init.xavier_uniform_(self.fc1.weight)
+        nn.init.xavier_uniform_(self.fc2.weight)
+        nn.init.normal_(self.fc1.bias, std=1e-6)
+        nn.init.normal_(self.fc2.bias, std=1e-6)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.act_fn(x)
+        x = self.fc2(x)
+        return x
+
+CONFIGS = {
+    'ViT-B_16': configs.get_b16_config(),
+    'ViT-B_32': configs.get_b32_config(),
+    'ViT-L_16': configs.get_l16_config(),
+    'ViT-L_32': configs.get_l32_config(),
+    'ViT-H_14': configs.get_h14_config(),
+    'R50-ViT-B_16': configs.get_r50_b16_config(),
+    'testing': configs.get_testing(),
+}
